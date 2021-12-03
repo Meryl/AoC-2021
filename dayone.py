7742408a2b3f3be5c0b0fd23d7541eac352e233e
Tim Loots <tim@go2people.nl>
2021-12-03 14:02:19 +0100

    Refactored days 1 and 2 somewhat

--- dayone.py
+++ dayone.py
@@ -1,21 +1,41 @@
-increases = 0
-decreases = 0
-prev      = None
+def partOne():
+    increases = 0
+    decreases = 0
+    prev      = None
 
-# Open the input file, use with so it's closed automatically
-with open('input/dayone.txt', 'r') as inputFile:
-    # Split on newlines, then apply int(x) for each line
-    lines = [int(x) for x in inputFile.read().splitlines()]
+    # Open the input file, use with so it's closed automatically
+    with open('input/dayone.txt', 'r') as inputFile:
+        # Split on newlines, then apply int(x) for each line
+        lines = [int(x) for x in inputFile.read().splitlines()]
 
-    # enumerate() is used for easy access to the index, basically equivalent
-    # to foreach ($list as $i => $line) with $list being an array that is a list
-    for i, line in enumerate(lines):
-        if (i > 0):
-            if (line > prev):
-                increases += 1
-            else:
-                decreases += 1
+        # enumerate() is used for easy access to the index, basically equivalent
+        # to foreach ($list as $i => $line) with $list being an array that is a list
+        for i, line in enumerate(lines):
+            if (i > 0):
+                if (line > prev):
+                    increases += 1
+                else:
+                    decreases += 1
 
-        prev = line
+            prev = line
 
-print("{} increases, {} decreases".format(increases, decreases))
+    print("Simple comparison: {} increases, {} decreases".format(increases, decreases))
+
+def partTwo():
+    increases = 0
+    decreases = 0
+
+    with open('input/dayone.txt', 'r') as inputFile:
+        lines = [int(x) for x in inputFile.read().splitlines()]
+
+        for i, line in enumerate(lines):
+            if (i > 2):
+                if (sum(lines[i - 3 : i]) < sum(lines[i - 2 : i + 1])):
+                    increases += 1
+                else:
+                    decreases += 1
+
+    print("Rolling window   : {} increases, {} decreases".format(increases, decreases))
+
+partOne()
+partTwo()
--- daytwo.py
+++ daytwo.py
@@ -1,17 +1,42 @@
-distance = 0
-depth = 0
+def partOne():
+    distance = 0
+    depth = 0
 
-with open('input/daytwo.txt', 'r') as inputFile:
-    lines = inputFile.read().splitlines()
+    with open('input/daytwo.txt', 'r') as inputFile:
+        lines = inputFile.read().splitlines()
 
-    for line in lines:
-        cmd, amount = line.split()
+        for line in lines:
+            cmd, amount = line.split()
 
-        if (cmd == "forward"):
-            distance += int(amount)
-        elif (cmd == "down"):
-            depth += int(amount)
-        elif (cmd == "up"):
-            depth -= int(amount)
+            if (cmd == "forward"):
+                distance += int(amount)
+            elif (cmd == "down"):
+                depth += int(amount)
+            elif (cmd == "up"):
+                depth -= int(amount)
 
-print("Horizontal distance {}, depth {}, multiplied {}".format(distance, depth, distance * depth))
+    print("Horizontal distance {}, depth {}, multiplied {}".format(distance, depth, distance * depth))
+
+def partTwo():
+    distance = 0
+    depth = 0
+    aim = 0
+
+    with open('input/daytwo.txt', 'r') as inputFile:
+        lines = inputFile.read().splitlines()
+
+        for line in lines:
+            cmd, amount = line.split()
+
+            if (cmd == "forward"):
+                distance += int(amount)
+                depth += aim * int(amount)
+            elif (cmd == "down"):
+                aim += int(amount)
+            elif (cmd == "up"):
+                aim -= int(amount)
+
+    print("Horizontal distance {}, depth {}, multiplied {}".format(distance, depth, distance * depth))
+
+partOne()
+partTwo()
